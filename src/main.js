793e117644c1610230380ab6905c9f009cc0b46e
TahuTech <18053036@student.uwp.ac.id>
2022-07-05 15:45:02 +0700

    add dependency bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
 
 createApp(App).mount('#app')
