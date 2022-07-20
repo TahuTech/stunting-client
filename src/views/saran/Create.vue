d002f8081c40a2022eca13593e2e289b2a2c2e27
TahuTech <18053036@student.uwp.ac.id>
2022-07-20 19:51:45 +0700

    saran create and edit

--- src/views/saran/Create.vue
+++ src/views/saran/Create.vue
@@ -1,11 +1,67 @@
 <template>
-    <h3>
-    
-        Create
-    
-    </h3>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <router-link :to="{ name: 'saran.index' }" class="btn btn-warning btn-sm rounded shadow mb-3">
+                    Kembali
+                </router-link>
+
+                <div class="class card rounded shadow">
+                    <div class="card-header">Tambah Komentar</div>
+                    <div class="class card-body">
+                        <form @submit.prevent="store()">
+                            <div class="mb-3">
+                                <label for="" class="class form-label">Isi Saran</label>
+                                <input type="text" class="class form-control" v-model="saran.isi">
+                                <div v-if="validation.isi" class="text-danger">
+                                    {{ validation.isi[0] }}</div>
+                            </div>
+                            <button class="btn btn-outline-primary">Simpan</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
-export default {};
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
+export default {
+    setup() {
+        //data binding
+        const saran = reactive({
+            isi: '',
+        });
+
+        const validation = ref([]);
+
+        const router = useRouter();
+
+        function store() {
+            axios.post(
+                'http://127.0.0.1:8000/api/saran',
+                saran
+            )
+                .then(() => {
+                    router.push({
+                        name: 'saran.index'
+                    });
+                }).catch((err) => {
+                    validation.value = err.response.data
+                });
+        }
+
+        return {
+            saran,
+            validation,
+            router,
+            store
+        }
+    }
+}
 </script>
--- src/views/saran/Edit.vue
+++ src/views/saran/Edit.vue
@@ -1,11 +1,76 @@
 <template>
-    <h3>
-    
-        Edit
-    
-    </h3>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <router-link :to="{ name: 'saran.index' }" class="btn btn-warning btn-sm rounded shadow mb-3">
+                    Kembali
+                </router-link>
+
+                <div class="card rounded shadow">
+                    <div class="card-header">Edit Komentar</div>
+                    <div class="card-body">
+                        <form @submit.prevent="update()">
+                            <div class="mb-3">
+                                <label for="" class="form-label">Isi Saran</label>
+                                <input type="text" class="form-control" v-model="saran.isi">
+                                <div v-if="validation.isi" class="text-danger">{{ validation.isi[0] }}</div>
+
+                            </div>
+                            <button class="btn btn-outline-primary">Simpan Perubahan</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+import { reactive, ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import axios from 'axios'
+
+export default {
+    setup() {
+        //data binding
+        let saran = reactive({
+            isi: '',
+        });
+
+        const validation = ref([]);
+
+        const router = useRouter();
+        const route = useRoute();
+
+        onMounted(() => {
+            axios.get(`http://127.0.0.1:8000/api/saran/${route.params.id}`)
+                .then((result) => {
+                    saran.isi = result.data.data.isi
+                }).catch((err) => {
+                    console.log(err.response.data)
+                });
+        });
+
+        function update() {
+            axios.put(
+                `http://127.0.0.1:8000/api/saran/${route.params.id}`,
+                saran
+            )
+                .then(() => {
+                    router.push({
+                        name: 'saran.index'
+                    });
+                }).catch((err) => {
+                    validation.value = err.response.data
+                });
+        }
+
+        return {
+            saran,
+            validation,
+            router,
+            update
+        }
+    }
+}
 </script>
