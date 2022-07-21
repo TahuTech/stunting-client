e4bd5b24a95b35b354e6a1dc8863d8779d3024dc
TahuTech <18053036@student.uwp.ac.id>
2022-07-21 11:36:55 +0700

    create knn

--- src/views/knn/Create.vue
+++ src/views/knn/Create.vue
@@ -10,8 +10,37 @@
           <div class="card-header">Tambah Data Pengukuran Balita</div>
           <div class="card-body">
             <form @submit.prevent="store()">
+              <div class="mb-3">
+                <label for="" class="form-label">Id Balita</label>
+                <input type="number" class="form-control">
 
+                <label for="" class="form-label">Umur Balita</label>
+                <input type="number" class="form-control" min="0" max="60">
 
+                <label for="" class="form-label">Berat Balita</label>
+                <input type="number" class="form-control" min="0" max="60">
+
+                <label for="" class="form-label">Tinggi Balita</label>
+                <input type="number" class="form-control" min="0" max="60">
+
+                <label for="" class="form-label">Bulan Pengukuran</label>
+                <br></br>
+                <select name="cars" id="cars" form="carform">
+                  <option value="1">Januari</option>
+                  <option value="2">Februari</option>
+                  <option value="3">Maret</option>
+                  <option value="4">April</option>
+                  <option value="5">Mei</option>
+                  <option value="6">Juni</option>
+                  <option value="7">Juli</option>
+                  <option value="8">Agustus</option>
+                  <option value="9">September</option>
+                  <option value="10">Oktober</option>
+                  <option value="11">November</option>
+                  <option value="12">Desember</option>
+                </select>
+
+              </div>
               <button class="btn btn-outline-primary">Simpan</button>
             </form>
           </div>
