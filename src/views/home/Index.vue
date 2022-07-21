514d436a0e3863507ff02a6bed8ea32386b4bb98
TahuTech <18053036@student.uwp.ac.id>
2022-07-21 20:09:30 +0700

    home add

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,9 +1,26 @@
 <template>
-    <h3>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
 
-        HOME
 
-    </h3>
+                <h4>Klanting</h4>
+                <div class="card rounded shadow">
+                    <div class="card-body">
+                        <p> sebuah website dimana untuk membantu mengklafikasikan balita tergolong dalam gizi
+                            (BB/U) yaitu gizi lebih, gizi baik, gizi kurang, atau gizi buruk; Tinggi (TB/U) yaitu sangat
+                            pendek, pendek, normal, tinggi; serta berat (BB/TB) yaitu sangat kurus, kurus, normal, dan
+                            gemuk. </p>
+                    </div>
+                    <br>
+                    <br>
+                    <p style="text-align:center">Nur Ali Sholikhin </p>
+                    <p style="text-align:center">18053036
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
